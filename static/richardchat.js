51e3c540ac2bed8bc8437aeef45407760f77289a
RichardGrechko736 <162537734+richardgrechko@users.noreply.github.com>
2025-05-04 22:53:33 +0500

    Update richardchat.js

--- static/richardchat.js
+++ static/richardchat.js
@@ -4,4 +4,16 @@ var system = {
 }
 var userMessages = [];
 function addMessage(username,handle,message,registered) {
+	var chatbox = document.getElementById("chatbox"),
+		paragraph = document.createElement("p"),
+		user = document.createElement("user"),
+		handle = document.createElement("handle"),
+		msgElement = document.createElement("msg");
+	user.innerText = username;
+	handle.innerText = "(" + handle + ")";
+	msgElement.innerText = message;
+	paragraph.appendChild(handle);
+	paragraph.appendChild(user);
+	paragraph.appendChild(document.createTextNode(": "));
+	paragraph.appendChild(message);
 }
